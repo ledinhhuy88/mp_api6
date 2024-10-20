5d2c3c15864aebec8daf5d39d8ffd26c1f820c9e
Huy Le <huy.le@deciduous.vn>
2024-10-20 22:24:36 +0700

    set the versioning namespaces for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    # we are going to list our resources here
+    namespace :v1 do
+      # we are going to list our resources here
+    end
   end
 end
